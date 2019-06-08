60e4e98250dd439563a96edba57652147cb4fbe0
coderbot <coderbot16@gmail.com>
2019-06-07 18:10:51 -0700

    Add remaining tree types with most associated blocks

--- src/main/java/net/coderbot/funwoods/init/FunwoodsBlocks.java
+++ src/main/java/net/coderbot/funwoods/init/FunwoodsBlocks.java
@@ -1,33 +1,121 @@
 package net.coderbot.funwoods.init;
 
+import net.coderbot.funwoods.block.CustomDoorBlock;
 import net.coderbot.funwoods.block.CustomSaplingBlock;
 import net.coderbot.funwoods.block.CustomSaplingGenerator;
+import net.coderbot.funwoods.block.CustomStairsBlock;
 import net.coderbot.funwoods.feature.CypressTreeFeature;
 import net.coderbot.funwoods.feature.RubberTreeFeature;
 import net.coderbot.funwoods.feature.TreeDefinition;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 
 public class FunwoodsBlocks {
+	public static WoodBlocks RUBBER;
+	public static WoodBlocks CYPRESS;
+	public static WoodBlocks BALD_CYPRESS;
+	public static WoodBlocks JAPANESE_MAPLE;
+	public static WoodBlocks RAINBOW_EUCALYPTUS;
+	public static WoodBlocks SAKURA;
+
 	public static CustomSaplingBlock RUBBER_SAPLING;
 	public static CustomSaplingBlock CYPRESS_SAPLING;
+	public static CustomSaplingBlock BALD_CYPRESS_SAPLING;
+	public static CustomSaplingBlock JAPANESE_MAPLE_SAPLING;
+	public static CustomSaplingBlock RAINBOW_EUCALYPTUS_SAPLING;
+	public static CustomSaplingBlock SAKURA_SAPLING;
 
 	public static void init() {
-		TreeDefinition.Basic oak = new TreeDefinition.Basic();
-		oak.leaves = Blocks.OAK_LEAVES.getDefaultState();
-		oak.wood = Blocks.OAK_LOG.getDefaultState();
+		//TreeDefinition.Basic oak = new TreeDefinition.Basic();
+		//oak.leaves = Blocks.OAK_LEAVES.getDefaultState();
+		//oak.wood = Blocks.OAK_LOG.getDefaultState();
 
-		RUBBER_SAPLING = Registry.register(Registry.BLOCK, "funwoods:rubber_sapling", new CustomSaplingBlock (
+		RUBBER = WoodBlocks.register("rubber");
+		CYPRESS = WoodBlocks.register("cypress");
+		BALD_CYPRESS = WoodBlocks.register("bald_cypress");
+		JAPANESE_MAPLE = WoodBlocks.register("japanese_maple");
+		RAINBOW_EUCALYPTUS = WoodBlocks.register("rainbow_eucalyptus");
+		SAKURA = WoodBlocks.register("sakura");
+
+		RUBBER_SAPLING = register("rubber_sapling", new CustomSaplingBlock (
 				new CustomSaplingGenerator (
-						rand -> new RubberTreeFeature(DefaultFeatureConfig::deserialize, true, oak))
+						rand -> new RubberTreeFeature(DefaultFeatureConfig::deserialize, true, RUBBER.getBasicDefinition()))
 				)
 		);
 
-		CYPRESS_SAPLING = Registry.register(Registry.BLOCK, "funwoods:cypress_sapling", new CustomSaplingBlock (
+		CYPRESS_SAPLING = register("cypress_sapling", new CustomSaplingBlock (
 				new CustomSaplingGenerator (
-						rand -> new CypressTreeFeature(DefaultFeatureConfig::deserialize, true, oak))
+						rand -> new CypressTreeFeature(DefaultFeatureConfig::deserialize, true, CYPRESS.getBasicDefinition()))
 				)
 		);
+
+		// TODO: These saplings need to be configured with proper tree generators
+		BALD_CYPRESS_SAPLING = register("bald_cypress_sapling", new CustomSaplingBlock (
+				new CustomSaplingGenerator (
+						rand -> new CypressTreeFeature(DefaultFeatureConfig::deserialize, true, BALD_CYPRESS.getBasicDefinition()))
+				)
+		);
+
+		JAPANESE_MAPLE_SAPLING = register("japanese_maple_sapling", new CustomSaplingBlock (
+				new CustomSaplingGenerator (
+						rand -> new CypressTreeFeature(DefaultFeatureConfig::deserialize, true, JAPANESE_MAPLE.getBasicDefinition()))
+				)
+		);
+
+		RAINBOW_EUCALYPTUS_SAPLING = register("rainbow_eucalyptus_sapling", new CustomSaplingBlock (
+				new CustomSaplingGenerator (
+						rand -> new CypressTreeFeature(DefaultFeatureConfig::deserialize, true, RAINBOW_EUCALYPTUS.getBasicDefinition()))
+				)
+		);
+
+		SAKURA_SAPLING = register("sakura_sapling", new CustomSaplingBlock (
+				new CustomSaplingGenerator (
+						rand -> new CypressTreeFeature(DefaultFeatureConfig::deserialize, true, SAKURA.getBasicDefinition()))
+				)
+		);
+	}
+
+	public static <T extends Block> T register(String name, T block) {
+		return Registry.register(Registry.BLOCK, new Identifier("funwoods", name), block);
+	}
+
+	public static class WoodBlocks {
+		LogBlock log;
+		LeavesBlock leaves;
+		Block planks;
+		SlabBlock slab;
+		CustomStairsBlock stairs;
+		FenceBlock fence;
+		FenceGateBlock fenceGate;
+		CustomDoorBlock door;
+
+		private WoodBlocks() {}
+
+		// TODO: Map colors
+		public static WoodBlocks register(String name) {
+			WoodBlocks blocks = new WoodBlocks();
+
+			blocks.log = FunwoodsBlocks.register(name + "_log", new LogBlock(MaterialColor.SPRUCE, Block.Settings.copy(Blocks.OAK_LOG)));
+			blocks.leaves = FunwoodsBlocks.register(name + "_leaves", new LeavesBlock(Block.Settings.copy(Blocks.OAK_LEAVES)));
+			blocks.planks = FunwoodsBlocks.register(name + "_planks", new Block(Block.Settings.copy(Blocks.OAK_PLANKS)));
+			blocks.slab = FunwoodsBlocks.register(name + "_slab", new SlabBlock(Block.Settings.copy(Blocks.OAK_SLAB)));
+			blocks.stairs = FunwoodsBlocks.register(name + "_stairs", new CustomStairsBlock(blocks.planks.getDefaultState(), Block.Settings.copy(Blocks.OAK_STAIRS)));
+			blocks.fence = FunwoodsBlocks.register(name + "_fence", new FenceBlock(Block.Settings.copy(Blocks.OAK_FENCE)));
+			blocks.fenceGate = FunwoodsBlocks.register(name + "_fence_gate", new FenceGateBlock(Block.Settings.copy(Blocks.OAK_FENCE_GATE)));
+			blocks.door = FunwoodsBlocks.register(name + "_door", new CustomDoorBlock(Block.Settings.copy(Blocks.OAK_FENCE_GATE)));
+
+			return blocks;
+		}
+
+		public TreeDefinition.Basic getBasicDefinition() {
+			TreeDefinition.Basic basic = new TreeDefinition.Basic();
+
+			basic.leaves = this.leaves.getDefaultState();
+			basic.wood = this.log.getDefaultState();
+
+			return basic;
+		}
 	}
 }
--- src/main/java/net/coderbot/funwoods/init/FunwoodsItems.java
+++ src/main/java/net/coderbot/funwoods/init/FunwoodsItems.java
@@ -4,18 +4,70 @@ import net.coderbot.funwoods.Funwoods;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class FunwoodsItems {
-	public static Item RUBBER_SAPLING;
-	public static Item CYPRESS_SAPLING;
+	public static WoodItems RUBBER;
+	public static WoodItems CYPRESS;
+	public static WoodItems BALD_CYPRESS;
+	public static WoodItems JAPANESE_MAPLE;
+	public static WoodItems RAINBOW_EUCALYPTUS;
+	public static WoodItems SAKURA;
+
+	public static BlockItem RUBBER_SAPLING;
+	public static BlockItem CYPRESS_SAPLING;
+	public static BlockItem BALD_CYPRESS_SAPLING;
+	public static BlockItem JAPANESE_MAPLE_SAPLING;
+	public static BlockItem RAINBOW_EUCALYPTUS_SAPLING;
+	public static BlockItem SAKURA_SAPLING;
 
 	public static void init() {
-		RUBBER_SAPLING = register("funwoods:rubber_sapling", FunwoodsBlocks.RUBBER_SAPLING);
-		CYPRESS_SAPLING = register("funwoods:cypress_sapling", FunwoodsBlocks.CYPRESS_SAPLING);
+		RUBBER = WoodItems.register("rubber", FunwoodsBlocks.RUBBER);
+		CYPRESS = WoodItems.register("cypress", FunwoodsBlocks.CYPRESS);
+		BALD_CYPRESS = WoodItems.register("bald_cypress", FunwoodsBlocks.BALD_CYPRESS);
+		JAPANESE_MAPLE = WoodItems.register("japanese_maple", FunwoodsBlocks.JAPANESE_MAPLE);
+		RAINBOW_EUCALYPTUS = WoodItems.register("rainbow_eucalyptus", FunwoodsBlocks.RAINBOW_EUCALYPTUS);
+		SAKURA = WoodItems.register("sakura", FunwoodsBlocks.SAKURA);
+
+		RUBBER_SAPLING = register("rubber_sapling", FunwoodsBlocks.RUBBER_SAPLING);
+		CYPRESS_SAPLING = register("cypress_sapling", FunwoodsBlocks.CYPRESS_SAPLING);
+		BALD_CYPRESS_SAPLING = register("bald_cypress_sapling", FunwoodsBlocks.BALD_CYPRESS_SAPLING);
+		JAPANESE_MAPLE_SAPLING = register("japanese_maple_sapling", FunwoodsBlocks.JAPANESE_MAPLE_SAPLING);
+		RAINBOW_EUCALYPTUS_SAPLING = register("rainbow_eucalyptus_sapling", FunwoodsBlocks.RAINBOW_EUCALYPTUS_SAPLING);
+		SAKURA_SAPLING = register("sakura_sapling", FunwoodsBlocks.SAKURA_SAPLING);
 	}
 
-	private static Item register(String name, Block block) {
-		return Registry.register(Registry.ITEM, name, new BlockItem(block, new Item.Settings().itemGroup(Funwoods.ITEM_GROUP)));
+	private static BlockItem register(String name, Block block) {
+		return Registry.register(Registry.ITEM, new Identifier("funwoods", name), new BlockItem(block, new Item.Settings().itemGroup(Funwoods.ITEM_GROUP)));
+	}
+
+	public static class WoodItems {
+		BlockItem log;
+		BlockItem leaves;
+		BlockItem planks;
+		BlockItem slab;
+		BlockItem stairs;
+		BlockItem fence;
+		BlockItem fenceGate;
+		BlockItem door;
+
+		private WoodItems() {}
+
+		public static WoodItems register(String name, FunwoodsBlocks.WoodBlocks blocks) {
+			WoodItems items = new WoodItems();
+
+			items.log = FunwoodsItems.register(name + "_log", blocks.log);
+			items.leaves = FunwoodsItems.register(name + "_leaves", blocks.leaves);
+			items.planks = FunwoodsItems.register(name + "_planks", blocks.planks);
+			items.slab = FunwoodsItems.register(name + "_slab", blocks.slab);
+			items.stairs = FunwoodsItems.register(name + "_stairs", blocks.stairs);
+			items.fence = FunwoodsItems.register(name + "_fence", blocks.fence);
+			items.fenceGate = FunwoodsItems.register(name + "_fence_gate", blocks.fenceGate);
+			items.door = FunwoodsItems.register(name + "_door", blocks.door);
+
+
+			return items;
+		}
 	}
 }
