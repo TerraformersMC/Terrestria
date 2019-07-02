0da83175f704c955fd3826d42b4a8e9d4d23814d
Brandon Davis <goldr1000ftw@gmail.com>
2019-07-01 23:29:10 -0500

    Made the feature more dynamic in nature and added major branches
    basically what this does is make the leaf layers have a generated number, and reorganizes a lot of the code, and also adds branches. I want to come up with a better way to generate branches for the future though.

--- src/main/java/net/coderbot/terrestria/feature/ConiferTreeFeature.java
+++ src/main/java/net/coderbot/terrestria/feature/ConiferTreeFeature.java
@@ -1,7 +1,9 @@
 package net.coderbot.terrestria.feature;
 
 import com.mojang.datafixers.Dynamic;
+import io.github.terraformersmc.terraform.util.Shapes;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.LogBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.MutableIntBoundingBox;
@@ -29,102 +31,134 @@ public class ConiferTreeFeature extends AbstractTreeFeature<DefaultFeatureConfig
 
 	@Override
 	public boolean generate(Set<BlockPos> blocks, ModifiableTestableWorld world, Random rand, BlockPos origin, MutableIntBoundingBox boundingBox) {
-		// Total trunk height
+		// Total leaf height
 		int height = rand.nextInt(12) + 32;
 
-		// How much "bare trunk" there will be.
+		// How much "bare trunk" there will be below the leaves.
 		int bareTrunkHeight = 8 + rand.nextInt(12);
 
 		// Maximum leaf radius.
-		int maxRadius = 14 + rand.nextInt(8);
+		int maxRadius = 6 + rand.nextInt(4);
 
+		//Number of layers of leaves
+		int layers = rand.nextInt(4) + 4;
+
+		//The number blocks that a layer will shrink in radius per step.
+		double shrinkAmount = 1.0; //1.0 for 1 block
+
+		//If the tree doesn't have enough room with it's current height before build limit
 		if(origin.getY() + height + 1 > 256 || origin.getY() < 1) {
 			return false;
 		}
 
-		BlockPos below = origin.down();
-
-		if(!isNaturalDirtOrGrass(world, below)) {
+		//If the ground below the sapling is good enough for a tree
+		if(!isNaturalDirtOrGrass(world, origin.down())) {
 			return false;
 		}
 
+		//If there is room for the tree
 		if(!checkForObstructions(world, origin, height, bareTrunkHeight, maxRadius)) {
 			return false;
 		}
 
+		//Set the block below the trunk to dirt (because vanilla does it)
 		setBlockState(blocks, world, origin.down(), Blocks.DIRT.getDefaultState(), boundingBox);
-		growTrunk(blocks, world, new BlockPos.Mutable(origin), height, boundingBox);
-		
-
-		BlockPos.Mutable pos;
-		//Small lil frill at the bottom
-		pos = new BlockPos.Mutable(origin).setOffset(Direction.UP, height/4);
-		growLeaves(blocks, world, pos, height/2, bareTrunkHeight, maxRadius/3, boundingBox);
-
-		//big boi on bottom
-		pos = new BlockPos.Mutable(origin).setOffset(Direction.UP, (height/4)+3);
-		growLeaves(blocks, world, pos, height/2, bareTrunkHeight, maxRadius, boundingBox);
-
-		//medium boi
-		pos = new BlockPos.Mutable(origin).setOffset(Direction.UP, height/2);
-		growLeaves(blocks, world, pos, height/2, bareTrunkHeight, maxRadius - (maxRadius / 3), boundingBox);
-
-		//smol boi on the tip
-		pos = new BlockPos.Mutable(origin).setOffset(Direction.UP, height/3);
-		growLeaves(blocks, world, pos, height/2, bareTrunkHeight, maxRadius/2, boundingBox);
+		growTrunk(blocks, world, new BlockPos.Mutable(origin), height+bareTrunkHeight, boundingBox);
+		growBranches(blocks, world, new BlockPos.Mutable(origin).setOffset(Direction.UP, bareTrunkHeight), height, maxRadius, shrinkAmount, layers, boundingBox);
+		growLeaves(blocks, world, new BlockPos.Mutable(origin).setOffset(Direction.UP, bareTrunkHeight), height, maxRadius, shrinkAmount, layers, boundingBox);
 
 		return true;
 	}
 
-	private void growLeaves(Set<BlockPos> blocks, ModifiableTestableWorld world, BlockPos.Mutable pos, int height, int bareTrunkHeight, int maxRadius, MutableIntBoundingBox boundingBox) {
+	private void growBranches(Set<BlockPos> blocks, ModifiableTestableWorld world, BlockPos.Mutable pos, int height, int maxRadius, double shrinkAmount, int layers, MutableIntBoundingBox boundingBox) {
 		int x = pos.getX();
 		int y = pos.getY();
 		int z = pos.getZ();
-		double radius;
-		double innerRadius;
+		int layerHeight = (height/layers);
+		int majorBranchLength = maxRadius - 2; //don't ask why, but this number just works for some reason
+		int minotBranchLength = (int)((majorBranchLength / 2.0)); //half of the major branch length
 
-		for(int dy = 0; dy < height; dy++) {
-
-			pos.set(x, y + dy, z);
-
-			radius = maxRadius * radiusFactor(dy + bareTrunkHeight, height + bareTrunkHeight);
-			innerRadius = (maxRadius/3.0) * radiusFactor(dy + bareTrunkHeight, height + bareTrunkHeight);
-			if (innerRadius < 0) {
-				innerRadius = 0;
-			}
-
-			if(radius < 0) {
-				continue;
-			}
-
-			io.github.terraformersmc.terraform.util.Shapes.canopyCircle(pos, radius, innerRadius, position -> {
-				if(AbstractTreeFeature.isAirOrLeaves(world, pos)) {
-					setBlockState(blocks, world, pos, tree.getLeaves(), boundingBox);
+		for (int layer = 0; layer < layers; layer++) {
+			pos.set(x, y + (layer*layerHeight), z);
+			majorBranchLength = (int) (majorBranchLength - (layer * (shrinkAmount/3)));
+			for (int maBr = 0; maBr < majorBranchLength; maBr++) {
+				BlockPos origin = pos.toImmutable();
+				pos.setOffset(Direction.WEST, majorBranchLength);
+				for(int xb = -majorBranchLength; xb <= majorBranchLength; xb++) {
+					if (isAirOrLeaves(world, pos)) {
+						setBlockState(blocks, world, pos, tree.getLog().with(LogBlock.AXIS, Direction.WEST.getAxis()), boundingBox);
+					}
+					pos.setOffset(Direction.EAST);
 				}
-			});
+				pos.set(origin);
+				pos.setOffset(Direction.NORTH, majorBranchLength);
+				for(int yb = -majorBranchLength; yb <= majorBranchLength; yb++) {
+					if (isAirOrLeaves(world, pos)) {
+						setBlockState(blocks, world, pos, tree.getLog().with(LogBlock.AXIS, Direction.NORTH.getAxis()), boundingBox);
+					}
+					pos.setOffset(Direction.SOUTH);
+				}
+				//reset the branches
+				pos.set(origin);
+			}
+
+			if(AbstractTreeFeature.isAirOrLeaves(world, pos)) {
+				setBlockState(blocks, world, pos, tree.getLog(), boundingBox);
+			}
 		}
 	}
 
-	private double radiusFactor(double x, double height) {
-		//makes the polynomial apply to values from 0-the height
-		x = x / height;
+	private void growLeaves(Set<BlockPos> blocks, ModifiableTestableWorld world, BlockPos.Mutable pos, int height, int maxRadius, double shrinkAmmount, int layers, MutableIntBoundingBox boundingBox) {
+		int x = pos.getX();
+		int y = pos.getY();
+		int z = pos.getZ();
+		int layerHeight = (height/layers);
+		int leafLayerHeight = 2*(height/layers);
 
-		// A 3rd-degree polynomial approximating the shape of a Conifer tree. from 0-1
-		return -0.6 * (x*x*x) + 1.96 * (x*x) - 2.37 * x + 1;
+		for (int layer = 0; layer < layers; layer++) {
+			for(int currentHeight = 0; currentHeight < leafLayerHeight; currentHeight++) {
+
+				pos.set(x, y + (layer*layerHeight) + currentHeight, z);
+
+				Shapes.canopyCircle(pos,
+						outerRadius(maxRadius - (layer * shrinkAmmount), currentHeight, leafLayerHeight),
+						innerRadius(maxRadius - (layer * shrinkAmmount), currentHeight, layerHeight),
+						position -> {
+					if(AbstractTreeFeature.isAirOrLeaves(world, pos)) {
+						setBlockState(blocks, world, pos, tree.getLeaves(), boundingBox);
+					}
+				});
+			}
+		}
 	}
 
-	private double innerRadiusFactor(double x, double height) {
-		//makes the polynomial apply to values from 0-the height
-		x = x / height;
+	/**
+	 * @param maxRadius the maximum radius for the leaves to generate
+	 * @param currentHeight the progress from 0 to the height that the leaves have generated (the x value of the polynomial)
+	 * @param height the target height for the leaf layer
+	 * @return the radius of the outside leaf layer at it's current y-height
+	 */
+	private double outerRadius(double maxRadius, double currentHeight, double height) {
+		double x = currentHeight / height;
+		// A 3rd-degree polynomial approximating the shape of a Conifer tree. from 0-1
+		return maxRadius * ((-0.6 * (x*x*x) + 1.96 * (x*x) - 2.37 * x) + 1);
+	}
 
-		// A 3rd-degree polynomial approximating the inner shape of a Conifer tree. from 0-1
-		return -3.24 * (x*x*x) + .25 * (x*x) - 2.98 * x + 1;
+	/**
+	 * @param maxRadius the maximum radius for the leaves to generate
+	 * @param currentHeight the progress from 0 to the height that the leaves have generated (the x value of the polynomial)
+	 * @param height the target height for the leaf layer
+	 * @return the radius of the inner leaf layer at it's current y-height
+	 */
+	private double innerRadius(double maxRadius, double currentHeight, double height) {
+		double x = currentHeight / height;
+		x = maxRadius * ((-3.24 * (x*x*x) + .25 * (x*x) - 2.98 * x) + 1) - 2.5;
+		return x < 0 ? 0 : x;
 	}
 
 	private void growTrunk(Set<BlockPos> blocks, ModifiableTestableWorld world, BlockPos.Mutable pos, int height, MutableIntBoundingBox boundingBox) {
 		for(int i = 0; i < height; i++) {
 			setBlockState(blocks, world, pos, tree.getLog(), boundingBox);
-
 			pos.setOffset(Direction.UP);
 		}
 	}
