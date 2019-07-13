2cb055313b07604e4afcc872a8c4ffb042fa321e
Brandon Davis <goldr1000ftw@gmail.com>
2019-07-13 17:30:05 -0500

    cypress forest test

--- src/main/java/net/coderbot/terrestria/biome/biomes/CypressForestBiome.java
+++ src/main/java/net/coderbot/terrestria/biome/biomes/CypressForestBiome.java
@@ -1,5 +1,7 @@
 package net.coderbot.terrestria.biome.biomes;
 
+import net.coderbot.terrestria.biome.TerrestriaBiome;
+import net.coderbot.terrestria.init.TerrestriaFeatures;
 import net.minecraft.entity.EntityCategory;
 import net.minecraft.entity.EntityType;
 import net.minecraft.world.biome.Biome;
@@ -8,8 +10,56 @@ import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.decorator.CountExtraChanceDecoratorConfig;
 import net.minecraft.world.gen.decorator.Decorator;
 import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.surfacebuilder.SurfaceBuilder;
+
+import static net.coderbot.terrestria.biome.TerrestriaBiome.DefaultFeature.*;
 
 public class CypressForestBiome extends Biome {
+	
+	public static Biome get() {
+		return TerrestriaBiome.builder()
+				.configuredSurfaceBuilder(SurfaceBuilder.DEFAULT, SurfaceBuilder.GRASS_CONFIG)
+				.precipitation(Biome.Precipitation.RAIN).category(Biome.Category.FOREST)
+				.depth(0.1F)
+				.scale(0.2F)
+				.temperature(0.7F)
+				.downfall(0.8F)
+				.waterColor(4159204)
+				.waterFogColor(329011)
+				.addDefaultFeatures(LAND_CARVERS, STRUCTURES, LAKES, DUNGEONS, FOREST_FLOWERS, MINEABLES, ORES, DISKS,
+						DEFAULT_FLOWERS, DEFAULT_MUSHROOMS, FOREST_GRASS, DEFAULT_VEGETATION, SPRINGS, FROZEN_TOP_LAYER)
+				.addTerrestriaFeature(
+						GenerationStep.Feature.VEGETAL_DECORATION,
+						Biome.configureFeature(
+								Feature.RANDOM_SELECTOR,
+								new RandomFeatureConfig(
+										new Feature[]{TerrestriaFeatures.CYPRESS_TREE, TerrestriaFeatures.TALLER_BIRCH_TREE },
+										new FeatureConfig[]{ FeatureConfig.DEFAULT, FeatureConfig.DEFAULT },
+										new float[]{ 0.3F, 0.3F },
+
+										TerrestriaFeatures.CYPRESS_TREE,
+										FeatureConfig.DEFAULT
+								),
+								Decorator.COUNT_EXTRA_HEIGHTMAP,
+								new CountExtraChanceDecoratorConfig(13, 0.1F, 1)
+						))
+				.addSpawnEntry(new Biome.SpawnEntry(EntityType.SHEEP, 12, 4, 4))
+				.addSpawnEntry(new Biome.SpawnEntry(EntityType.PIG, 10, 4, 4))
+				.addSpawnEntry(new Biome.SpawnEntry(EntityType.CHICKEN, 10, 4, 4))
+				.addSpawnEntry(new Biome.SpawnEntry(EntityType.COW, 8, 4, 4))
+				.addSpawnEntry(new Biome.SpawnEntry(EntityType.WOLF, 5, 4, 4))
+				.addSpawnEntry(new Biome.SpawnEntry(EntityType.BAT, 10, 8, 8))
+				.addSpawnEntry(new Biome.SpawnEntry(EntityType.SPIDER, 100, 4, 4))
+				.addSpawnEntry(new Biome.SpawnEntry(EntityType.ZOMBIE, 95, 4, 4))
+				.addSpawnEntry(new Biome.SpawnEntry(EntityType.ZOMBIE_VILLAGER, 5, 1, 1))
+				.addSpawnEntry(new Biome.SpawnEntry(EntityType.SKELETON, 100, 4, 4))
+				.addSpawnEntry(new Biome.SpawnEntry(EntityType.CREEPER, 100, 4, 4))
+				.addSpawnEntry(new Biome.SpawnEntry(EntityType.SLIME, 100, 4, 4))
+				.addSpawnEntry(new Biome.SpawnEntry(EntityType.ENDERMAN, 10, 1, 4))
+				.addSpawnEntry(new Biome.SpawnEntry(EntityType.WITCH, 5, 1, 1))
+				.build();
+	}
+	
 	public CypressForestBiome(Biome.Settings settings, int trees, Feature<DefaultFeatureConfig> tree1, Feature<DefaultFeatureConfig> tree2, Feature<DefaultFeatureConfig> tree3) {
 		super(settings);
 
