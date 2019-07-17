1dd641203e041c7d29a1b27571fddacb0471c5e8
coderbot <coderbot16@gmail.com>
2019-07-17 08:04:19 -0700

    Fix up bugs in the glacier surface builder

--- src/main/java/net/coderbot/terrestria/surface/GlacierSurfaceBuilder.java
+++ src/main/java/net/coderbot/terrestria/surface/GlacierSurfaceBuilder.java
@@ -38,67 +38,50 @@ public class GlacierSurfaceBuilder extends SurfaceBuilder<TernarySurfaceConfig>
 			noiseGenerator = new SimplexNoiseSampler(new Random(seed));
 			currentSeed = seed;
 		}
+
 		double noise = noiseGenerator.sample((double) x / 260D, (double) z / 260D);
 		int glacierDifference = (int) ((noise > 0.22D && noise < 0.3D) ? (1171875 * Math.pow(noise - 0.26, 4)) - 3 : 0);
-		//System.out.println(String.valueOf(noise) + " -> " + String.valueOf(glacierDifference));
 		
 		int localX = x & 15;
 		int localZ = z & 15;
-		
-		int height = getHeightAtPos(chunk, localX, localZ);
-		
-		if (glacierDifference != 0) {
-			height -= 2;
-		}
-		
+
 		BlockPos.Mutable pos = new BlockPos.Mutable(localX, 255, localZ);
-		
+
+		if (glacierDifference != 0) {
+			pos.setY(worldHeight);
+			chunk.setBlockState(pos, AIR, false);
+
+			pos.setY(worldHeight - 1);
+			chunk.setBlockState(pos, AIR, false);
+
+			worldHeight -= 2;
+		}
+
 		for (int y = worldHeight; y >= 0; --y)
 		{
 			pos.setY(y);
 			BlockState toSet = STONE;
-			
-			if (y <= height) {
-				if (y - height >= glacierDifference) {
-					System.out.print(x);
-					System.out.print(":");
-					System.out.println(z);
-					System.out.println();
-					toSet = (y - height == -1) ? bICE : pICE;
-				} else {
-					if (y < 255) {
-						BlockState upState = chunk.getBlockState(pos.up());
-						if (upState == AIR) {
-							toSet = config.getTopMaterial();
-						} else if (upState == WATER) {
-							toSet = config.getUnderwaterMaterial();
-						} else {
-							if (y < 253) {
-								if (chunk.getBlockState(pos.up(3)) == AIR || chunk.getBlockState(pos.up(2)) == AIR) {
-									toSet = config.getUnderMaterial();
-								}
+
+			if (y - worldHeight > glacierDifference) {
+				toSet = (y - worldHeight == -1) ? bICE : pICE;
+			} else {
+				if (y < 255) {
+					BlockState upState = chunk.getBlockState(pos.up());
+					if (upState == AIR) {
+						toSet = config.getTopMaterial();
+					} else if (upState == WATER) {
+						toSet = config.getUnderwaterMaterial();
+					} else {
+						if (y < 253) {
+							if (chunk.getBlockState(pos.up(3)) == AIR || chunk.getBlockState(pos.up(2)) == AIR) {
+								toSet = config.getUnderMaterial();
 							}
 						}
 					}
 				}
-			} else {
-				toSet = y < 63 ? WATER : AIR;
 			}
-			
+
 			chunk.setBlockState(pos, toSet, false);
 		}
 	}
-	
-	private static int getHeightAtPos(Chunk chunk, int x, int z) {
-		BlockPos.Mutable pos = new BlockPos.Mutable(x, 255, z);
-		for (int i = 255; i > 0; ++i) {
-			pos.setY(i);
-			BlockState state = chunk.getBlockState(pos);
-			if (state != AIR) {
-				return i;
-			}
-		}
-		return 0;
-	}
-
 }
