cddec739a9bed79fd838894225697fcc769004b5
Brandon Davis <goldr1000ftw@gmail.com>
2019-07-19 16:56:57 -0500

    and in the features

--- src/main/java/net/coderbot/terrestria/init/TerrestriaFeatures.java
+++ src/main/java/net/coderbot/terrestria/init/TerrestriaFeatures.java
@@ -146,7 +146,7 @@ public class TerrestriaFeatures {
 		// TODO: palm wood
 		TreeDefinition.Basic palmDefinition = new TreeDefinition.Basic(
 				Blocks.JUNGLE_LOG.getDefaultState(),
-				TerrestriaBlocks.PALM.leaves.getDefaultState()
+				Blocks.JUNGLE_LEAVES.getDefaultState()
 		);
 
 		PALM_TREE = register("palm_tree",
