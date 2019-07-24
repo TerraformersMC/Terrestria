6962d8594503bbc4860f54b814d1be4877e66d89
coderbot <coderbot16@gmail.com>
2019-07-24 15:07:22 -0700

    Moved biome configuration system to Terraform

--- src/main/java/com/terraformersmc/terrestria/Terrestria.java
+++ src/main/java/com/terraformersmc/terrestria/Terrestria.java
@@ -1,8 +1,8 @@
 package com.terraformersmc.terrestria;
 
+import com.terraformersmc.terraform.config.BiomeConfig;
+import com.terraformersmc.terraform.config.BiomeConfigHandler;
 import com.terraformersmc.terrestria.command.LocateAny;
-import com.terraformersmc.terrestria.config.BiomeConfig;
-import com.terraformersmc.terrestria.config.BiomeConfigHandler;
 import com.terraformersmc.terrestria.init.*;
 import com.terraformersmc.terrestria.item.LogTurnerItem;
 import net.fabricmc.api.ModInitializer;
@@ -16,12 +16,14 @@ import net.minecraft.util.registry.Registry;
 public class Terrestria implements ModInitializer {
 	public static final String MOD_ID = "terrestria";
 	public static ItemGroup ITEM_GROUP;
+	public static BiomeConfigHandler BIOME_CONFIG_HANDLER;
 
 	@Override
 	public void onInitialize() {
 		ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "items"), () -> new ItemStack(TerrestriaItems.RUBBER_SAPLING));
+		BIOME_CONFIG_HANDLER = new BiomeConfigHandler(MOD_ID);
 
-		BiomeConfig config = BiomeConfigHandler.getBiomeConfig();
+		BiomeConfig config = BIOME_CONFIG_HANDLER.getBiomeConfig();
 
 		TerrestriaBlocks.init();
 		TerrestriaItems.init();
@@ -30,7 +32,7 @@ public class Terrestria implements ModInitializer {
 		TerrestriaBiomes.init();
 		TerrestriaGeneration.init(config);
 
-		BiomeConfigHandler.save();
+		BIOME_CONFIG_HANDLER.save();
 
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "log_turner"), new LogTurnerItem(new Item.Settings().group(ITEM_GROUP)));
 		LocateAny.register();
