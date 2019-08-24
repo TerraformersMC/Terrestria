bf6641143ed02cad7688f29c4ec19903c9aa29ce
coderbot <coderbot16@gmail.com>
2019-08-23 22:54:27 -0700

    First pass at boats

--- src/main/java/com/terraformersmc/terrestria/init/helpers/TerrestriaRegistry.java
+++ src/main/java/com/terraformersmc/terrestria/init/helpers/TerrestriaRegistry.java
@@ -1,25 +1,45 @@
 package com.terraformersmc.terrestria.init.helpers;
 
+import com.terraformersmc.terraform.entity.TerraformBoatEntity;
+import com.terraformersmc.terraform.item.TerraformBoatItem;
 import com.terraformersmc.terraform.util.RecipeUtil;
 import com.terraformersmc.terrestria.Terrestria;
 import net.minecraft.block.Block;
+import net.minecraft.entity.EntityType;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.SignItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
+import java.util.function.Supplier;
+
 public class TerrestriaRegistry {
 
 	public static BlockItem registerBlockItem(String name, Block block) {
 		BlockItem item = new BlockItem(block, new Item.Settings().group(Terrestria.ITEM_GROUP));
 		item.appendBlocks(Item.BLOCK_ITEMS, item);
+
 		RecipeUtil.registerCompostableBlock(block);
-		return Registry.register(net.minecraft.util.registry.Registry.ITEM, new Identifier(Terrestria.MOD_ID, name), item);
+
+		return Registry.register(Registry.ITEM, new Identifier(Terrestria.MOD_ID, name), item);
 	}
 
 	public static SignItem registerSignItem(String name, Block standing, Block wall) {
-		return Registry.register(net.minecraft.util.registry.Registry.ITEM, new Identifier(Terrestria.MOD_ID, name), new SignItem(new Item.Settings().group(Terrestria.ITEM_GROUP), standing, wall));
+		return Registry.register(Registry.ITEM, new Identifier(Terrestria.MOD_ID, name), new SignItem(new Item.Settings().group(Terrestria.ITEM_GROUP), standing, wall));
+	}
+
+	public static TerraformBoatItem registerBoatItem(String name, Supplier<EntityType<TerraformBoatEntity>> boatType) {
+		return Registry.register(Registry.ITEM, new Identifier(Terrestria.MOD_ID, name), new TerraformBoatItem (
+			(world, x, y, z) -> {
+				TerraformBoatEntity entity = boatType.get().create(world);
+
+				entity.setPosition(x, y, z);
+
+				return entity;
+			},
+			new Item.Settings().group(Terrestria.ITEM_GROUP)
+		));
 	}
 
 	public static <T extends Block> T register(String name, T block) {
