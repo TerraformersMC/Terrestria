2dbc03dc6b6d3c728d41bfb73ac5a91e15df78fd
Prospector <prospectordev@gmail.com>
2020-04-16 02:30:12 -0700

    20w16a #change "Ported to 20w16a" #release beta

--- src/main/java/com/terraformersmc/terrestria/feature/volcano/VolcanoStructureFeature.java
+++ src/main/java/com/terraformersmc/terrestria/feature/volcano/VolcanoStructureFeature.java
@@ -4,9 +4,9 @@ import com.mojang.datafixers.Dynamic;
 import com.terraformersmc.terrestria.init.TerrestriaBiomes;
 import net.minecraft.structure.StructureManager;
 import net.minecraft.structure.StructureStart;
+import net.minecraft.util.math.BlockBox;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
-import net.minecraft.util.math.BlockBox;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.source.BiomeAccess;
 import net.minecraft.world.gen.ChunkRandom;
@@ -29,7 +29,27 @@ public class VolcanoStructureFeature extends StructureFeature<DefaultFeatureConf
 		super(function);
 	}
 
+
+
 	@Override
+	protected boolean shouldStartAt(BiomeAccess biomeAccess, ChunkGenerator<?> generator, ChunkRandom random, int chunkX, int chunkZ, Biome biome, ChunkPos chunkPos) {
+		ChunkPos start = this.getStart(generator, random, chunkX, chunkZ, 0, 0);
+
+		if (chunkX == start.x && chunkZ == start.z) {
+			Biome biome1 = biomeAccess.getBiome(new BlockPos(chunkX * 16 + 9, 0, chunkZ * 16 + 9));
+
+			if (biome1.getCategory() == Biome.Category.OCEAN && random.nextInt(4) != 0) {
+				return false;
+			} else if (biome1 == TerrestriaBiomes.VOLCANIC_ISLAND_SHORE && random.nextInt(2) != 0) {
+				return false;
+			}
+
+			return generator.hasStructure(biome1, this);
+		}
+
+		return false;
+	}
+
 	protected ChunkPos getStart(ChunkGenerator<?> chunkGenerator_1, Random random_1, int chunkX, int chunkZ, int scaleX, int scaleZ) {
 		chunkX += VOLCANO_SPACING * scaleX;
 		chunkZ += VOLCANO_SPACING * scaleZ;
@@ -50,25 +70,6 @@ public class VolcanoStructureFeature extends StructureFeature<DefaultFeatureConf
 		finalChunkZ += random_1.nextInt(VOLCANO_SPACING - VOLCANO_SEPARATION);
 
 		return new ChunkPos(finalChunkX, finalChunkZ);
-	}
-
-	@Override
-	public boolean shouldStartAt(BiomeAccess biomeAccess, ChunkGenerator<?> generator, Random random, int chunkX, int chunkZ, Biome biomeIn) {
-		ChunkPos start = this.getStart(generator, random, chunkX, chunkZ, 0, 0);
-
-		if (chunkX == start.x && chunkZ == start.z) {
-			Biome biome = biomeAccess.getBiome(new BlockPos(chunkX * 16 + 9, 0, chunkZ * 16 + 9));
-
-			if (biome.getCategory() == Biome.Category.OCEAN && random.nextInt(4) != 0) {
-				return false;
-			} else if (biome == TerrestriaBiomes.VOLCANIC_ISLAND_SHORE && random.nextInt(2) != 0) {
-				return false;
-			}
-
-			return generator.hasStructure(biome, this);
-		}
-
-		return false;
 	}
 
 	@Override
