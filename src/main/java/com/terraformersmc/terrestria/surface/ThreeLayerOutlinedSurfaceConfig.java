baa8f29a594c3ac00c8da0d869fd2931d3204d10
Brandon Davis <neusfear@gmail.com>
2020-06-24 13:42:05 -0500

    Thanks SuperCodec

--- src/main/java/com/terraformersmc/terrestria/surface/ThreeLayerOutlinedSurfaceConfig.java
+++ src/main/java/com/terraformersmc/terrestria/surface/ThreeLayerOutlinedSurfaceConfig.java
@@ -3,8 +3,6 @@ package com.terraformersmc.terrestria.surface;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.world.gen.stateprovider.BlockStateProvider;
 import net.minecraft.world.gen.surfacebuilder.TernarySurfaceConfig;
 
 public class ThreeLayerOutlinedSurfaceConfig extends TernarySurfaceConfig {
@@ -38,18 +36,13 @@ public class ThreeLayerOutlinedSurfaceConfig extends TernarySurfaceConfig {
 		return outerNoisePoint;
 	}
 
-	public static Codec<ThreeLayerOutlinedSurfaceConfig> CODEC = RecordCodecBuilder.create((instance) -> {
-		return instance.group(BlockStateProvider.CODEC.fieldOf("top_middle").forGetter((threeLayerOutlinedSurfaceConfig) -> {
-			return threeLayerOutlinedSurfaceConfig.topMiddle;
-		})).apply(instance, ThreeLayerOutlinedSurfaceConfig::new);
-	});
-
-/*	public static ThreeLayerOutlinedSurfaceConfig CODEC = new Codec {
-		TernarySurfaceConfig ternary = TernarySurfaceConfig.deserialize(dynamic);
-		BlockState topMiddle = (BlockState)dynamic.get("top_middle").map(BlockState::deserialize).orElse(Blocks.AIR.getDefaultState());
-		float middleNoise = dynamic.get("middle_noise_point").asFloat(0.5F);
-		BlockState topEdge = (BlockState)dynamic.get("top_outer").map(BlockState::deserialize).orElse(Blocks.AIR.getDefaultState());
-		float outerNoise = dynamic.get("outer_noise_point").asFloat(0.7F);
-		return new ThreeLayerOutlinedSurfaceConfig(ternary.getTopMaterial(), topMiddle, middleNoise, topEdge, outerNoise, ternary.getUnderMaterial(), ternary.getUnderwaterMaterial());
-	}*/
+	public static Codec<ThreeLayerOutlinedSurfaceConfig> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+			BlockState.CODEC.fieldOf("top_material").forGetter(TernarySurfaceConfig::getTopMaterial),
+			BlockState.CODEC.fieldOf("top_middle").forGetter(config -> config.topMiddle),
+			Codec.FLOAT.fieldOf("middle_noise_point").forGetter(config -> config.middleNoisePoint),
+			BlockState.CODEC.fieldOf("top_outer").forGetter(config -> config.topOuter),
+			Codec.FLOAT.fieldOf("outer_noise_point").forGetter(config -> config.outerNoisePoint),
+			BlockState.CODEC.fieldOf("under_material").forGetter(TernarySurfaceConfig::getUnderMaterial),
+			BlockState.CODEC.fieldOf("underwater_material").forGetter(TernarySurfaceConfig::getUnderwaterMaterial))
+			.apply(instance, ThreeLayerOutlinedSurfaceConfig::new));
 }
