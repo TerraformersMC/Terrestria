2d0ec9d2e74ee3b71bc69df2bf0e09b1397feb68
Brandon Davis <neusfear@gmail.com>
2020-06-25 12:39:33 -0500

    Fix fallen log placement issues

--- src/main/java/com/terraformersmc/terrestria/feature/placer/trunk/FallenStraightTrunkPlacer.java
+++ src/main/java/com/terraformersmc/terrestria/feature/placer/trunk/FallenStraightTrunkPlacer.java
@@ -1,6 +1,9 @@
 package com.terraformersmc.terrestria.feature.placer.trunk;
 
 import com.google.common.collect.ImmutableList;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.PillarBlock;
 import net.minecraft.util.math.BlockBox;
 import net.minecraft.util.math.BlockPos;
@@ -28,26 +31,40 @@ public class FallenStraightTrunkPlacer extends StraightTrunkPlacer {
 
 	@Override
 	public List<FoliagePlacer.TreeNode> generate(ModifiableTestableWorld world, Random random, int trunkHeight, BlockPos pos, Set<BlockPos> set, BlockBox blockBox, TreeFeatureConfig treeFeatureConfig) {
-		method_27400(world, pos.down());
 
-		// Axis
-		Direction.Axis axis = random.nextBoolean() ? Direction.Axis.X : Direction.Axis.Z;
-		Direction direction = Direction.from(axis, random.nextBoolean() ? Direction.AxisDirection.POSITIVE : Direction.AxisDirection.NEGATIVE);
+		// Select a Direction for the log to be placed in
+		Direction direction = random.nextBoolean() ? Direction.NORTH : Direction.EAST;
 
-		for (int i = 0; i < trunkHeight; ++i) {
-			placeTrunkBlock(world, random, pos.offset(direction, i), set, blockBox, treeFeatureConfig, axis);
+		//Create the Mutable version of our block position so that we can procedurally create the log
+		BlockPos.Mutable currentPosition = pos.mutableCopy().move(direction, trunkHeight / 2 + 1);
+
+		//Determine the Percentage of the ground that the log will lay flat on
+		int numFlat = 0;
+		for (int i = 0; i < trunkHeight; i++) {
+			if (world.testBlockState(currentPosition.move(direction.getOpposite()).down(), blockState -> {
+				Block block = blockState.getBlock();
+				return block != Blocks.AIR;
+			})) {
+				numFlat++;
+			}
 		}
 
-		return ImmutableList.of(new FoliagePlacer.TreeNode(pos.up(trunkHeight), 0, false));
+		//If 60% of the blocks are supported and flat
+		if ((double)numFlat / trunkHeight > 0.6) {
+			//Place the blocks
+			for (int i = 0; i < trunkHeight; ++i) {
+				checkAndPlaceSpecificBlockState(world, random, currentPosition.move(direction), set, blockBox, treeFeatureConfig.trunkProvider.getBlockState(random, currentPosition).with(PillarBlock.AXIS, direction.getAxis()));
+			}
+		}
+
+		//No foliage placer locations needed
+		return ImmutableList.of();
 	}
 
-	protected static boolean placeTrunkBlock(ModifiableTestableWorld modifiableTestableWorld, Random random, BlockPos blockPos, Set<BlockPos> set, BlockBox blockBox, TreeFeatureConfig treeFeatureConfig, Direction.Axis axis) {
+	private static void checkAndPlaceSpecificBlockState(ModifiableTestableWorld modifiableTestableWorld, Random random, BlockPos blockPos, Set<BlockPos> set, BlockBox blockBox, BlockState blockState) {
 		if (TreeFeature.canReplace(modifiableTestableWorld, blockPos)) {
-			method_27404(modifiableTestableWorld, blockPos, treeFeatureConfig.trunkProvider.getBlockState(random, blockPos).with(PillarBlock.AXIS, axis), blockBox);
+			method_27404(modifiableTestableWorld, blockPos, blockState, blockBox);
 			set.add(blockPos.toImmutable());
-			return true;
-		} else {
-			return false;
 		}
 	}
 }
