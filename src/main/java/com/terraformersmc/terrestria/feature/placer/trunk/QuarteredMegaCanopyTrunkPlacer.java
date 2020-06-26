f008641c3b16ff215e96cae59cf23c7ecfe84dd5
Brandon Davis <neusfear@gmail.com>
2020-06-26 11:40:26 -0500

    Auto stash before merge of "update/dry/update-purge" and "origin/update/dry/update-purge"

---
+++ src/main/java/com/terraformersmc/terrestria/feature/placer/trunk/QuarteredMegaCanopyTrunkPlacer.java
@@ -0,0 +1,124 @@
+package com.terraformersmc.terrestria.feature.placer.trunk;
+
+import com.google.common.collect.ImmutableList;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import com.terraformersmc.terraform.block.QuarterLogBlock;
+import com.terraformersmc.terraform.helpers.DirectionHelper;
+import com.terraformersmc.terrestria.feature.treeconfigs.QuarteredMegaTreeConfig;
+import com.terraformersmc.terrestria.init.TerrestriaTrunkPlacerTypes;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockBox;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.ModifiableTestableWorld;
+import net.minecraft.world.gen.feature.TreeFeature;
+import net.minecraft.world.gen.feature.TreeFeatureConfig;
+import net.minecraft.world.gen.foliage.FoliagePlacer;
+import net.minecraft.world.gen.trunk.TrunkPlacer;
+import net.minecraft.world.gen.trunk.TrunkPlacerType;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+public class QuarteredMegaCanopyTrunkPlacer extends TrunkPlacer {
+
+	public static final Codec<QuarteredMegaCanopyTrunkPlacer> CODEC = RecordCodecBuilder.create(quarteredMegaCanopyTrunkPlacerInstance ->
+			method_28904(quarteredMegaCanopyTrunkPlacerInstance).apply(quarteredMegaCanopyTrunkPlacerInstance, QuarteredMegaCanopyTrunkPlacer::new));
+
+	public QuarteredMegaCanopyTrunkPlacer(int baseHeight, int firstRandomHeight, int secondRandomHeight) {
+		super(baseHeight, firstRandomHeight, secondRandomHeight);
+	}
+
+	@Override
+	protected TrunkPlacerType<?> getType() {
+		return TerrestriaTrunkPlacerTypes.QUARTERED_MEGA_CANOPY;
+	}
+
+	@Override
+	public List<FoliagePlacer.TreeNode> generate(ModifiableTestableWorld world, Random random, int trunkHeight, BlockPos pos, Set<BlockPos> set, BlockBox blockBox, TreeFeatureConfig treeFeatureConfig) {
+
+		//Check and set the block below to dirt
+		method_27400(world, pos.down());
+		method_27400(world, pos.down().east());
+		method_27400(world, pos.down().south());
+		method_27400(world, pos.down().south().east());
+
+		//Create the Mutable version of our block position so that we can procedurally create the trunk
+		BlockPos.Mutable currentPosition = pos.mutableCopy().move(Direction.DOWN);
+
+		//Create the placer storage
+		ArrayList<FoliagePlacer.TreeNode> foliageNodes = new ArrayList<>();
+
+		//Place the trunk without branches
+		for (int i = 0; i < trunkHeight; i++) {
+			placeLayer(world, random, currentPosition.move(Direction.UP).toImmutable(), set, blockBox, ((QuarteredMegaTreeConfig)treeFeatureConfig), false);
+		}
+
+		//Determine the number of branched layers to have
+		int layers = 5 + random.nextInt(3);
+
+		//Place the branched pieces
+		for (int i = 0; i < layers; i++) {
+			//Place the layers with branches and add the end of the branch to the foliage locations
+			foliageNodes.add(new FoliagePlacer.TreeNode(placeLayer(world, random, currentPosition.move(Direction.UP).toImmutable(), set, blockBox, ((QuarteredMegaTreeConfig)treeFeatureConfig), true), random.nextInt(2) + 4, true));
+		}
+
+		//Make sure the top of the tree has leaf locations
+		foliageNodes.add(new FoliagePlacer.TreeNode(placeBranch(world, random, currentPosition.toImmutable(), set, blockBox, ((QuarteredMegaTreeConfig)treeFeatureConfig), Direction.NORTH), random.nextInt(2) + 4, true));
+		foliageNodes.add(new FoliagePlacer.TreeNode(placeBranch(world, random, currentPosition.toImmutable(), set, blockBox, ((QuarteredMegaTreeConfig)treeFeatureConfig), Direction.SOUTH), random.nextInt(2) + 4, true));
+		foliageNodes.add(new FoliagePlacer.TreeNode(placeBranch(world, random, currentPosition.toImmutable(), set, blockBox, ((QuarteredMegaTreeConfig)treeFeatureConfig), Direction.EAST), random.nextInt(2) + 4, true));
+		foliageNodes.add(new FoliagePlacer.TreeNode(placeBranch(world, random, currentPosition.toImmutable(), set, blockBox, ((QuarteredMegaTreeConfig)treeFeatureConfig), Direction.WEST), random.nextInt(2) + 4, true));
+
+		//Return the nodes as an Immutable List to be placed later
+		return ImmutableList.copyOf(foliageNodes);
+	}
+
+	private BlockPos placeLayer(ModifiableTestableWorld world, Random random, BlockPos pos, Set<BlockPos> set, BlockBox blockBox, QuarteredMegaTreeConfig treeFeatureConfig, boolean placeBranches) {
+		checkAndPlaceSpecificBlockState(world, random, pos.south(), 		set, blockBox, treeFeatureConfig.quarterLogBlock.with(QuarterLogBlock.BARK_SIDE, QuarterLogBlock.BarkSide.SOUTHWEST));
+		checkAndPlaceSpecificBlockState(world, random, pos.east(), 			set, blockBox, treeFeatureConfig.quarterLogBlock.with(QuarterLogBlock.BARK_SIDE, QuarterLogBlock.BarkSide.NORTHEAST));
+		checkAndPlaceSpecificBlockState(world, random, pos.south().east(), 	set, blockBox, treeFeatureConfig.quarterLogBlock.with(QuarterLogBlock.BARK_SIDE, QuarterLogBlock.BarkSide.SOUTHEAST));
+		checkAndPlaceSpecificBlockState(world, random, pos, 				set, blockBox, treeFeatureConfig.quarterLogBlock.with(QuarterLogBlock.BARK_SIDE, QuarterLogBlock.BarkSide.NORTHWEST));
+
+		//Place a branch if we need one
+		if (placeBranches) {
+			//Determine the branch direction
+			Direction direction = Direction.Type.HORIZONTAL.random(random);
+			return placeBranch(world, random, pos, set, blockBox, treeFeatureConfig, direction);
+		} else {
+			//This should never get used but we have to return something
+			return pos;
+		}
+	}
+
+	private BlockPos placeBranch(ModifiableTestableWorld world, Random random, BlockPos pos, Set<BlockPos> set, BlockBox blockBox, QuarteredMegaTreeConfig treeFeatureConfig, Direction direction) {
+		//Create the Mutable version of our block position so that we can procedurally create the branch
+		BlockPos.Mutable currentPosition = pos.mutableCopy().move(Direction.DOWN);
+
+		//Pick weather this branch should go diagonal or straight
+		Direction diagonalDirection = DirectionHelper.randomHorizontalDirectionAwayFrom(random, direction.getOpposite());
+
+		//Determine the length of the branch
+		int length = 4 + random.nextInt(3);
+
+		//Place a branch with length in the diagonalDirection, with an upwards angle
+		for (int i = 0; i < length; i++) {
+			if (random.nextBoolean()) {
+				method_27402(world, random, currentPosition.move(direction), set, blockBox, treeFeatureConfig);
+				method_27402(world, random, currentPosition.move(diagonalDirection), set, blockBox, treeFeatureConfig);
+			}
+			method_27402(world, random, currentPosition.move(Direction.UP), set, blockBox, treeFeatureConfig);
+		}
+
+		return currentPosition.toImmutable();
+	}
+
+	private static void checkAndPlaceSpecificBlockState(ModifiableTestableWorld modifiableTestableWorld, Random random, BlockPos blockPos, Set<BlockPos> set, BlockBox blockBox, BlockState blockState) {
+		if (TreeFeature.canReplace(modifiableTestableWorld, blockPos)) {
+			method_27404(modifiableTestableWorld, blockPos, blockState, blockBox);
+			set.add(blockPos.toImmutable());
+		}
+	}
+}
--- src/main/java/com/terraformersmc/terrestria/init/TerrestriaTrunkPlacerTypes.java
+++ src/main/java/com/terraformersmc/terrestria/init/TerrestriaTrunkPlacerTypes.java
@@ -3,10 +3,7 @@ package com.terraformersmc.terrestria.init;
 import com.mojang.serialization.Codec;
 import com.terraformersmc.terraform.placer.PlacerTypes;
 import com.terraformersmc.terrestria.Terrestria;
-import com.terraformersmc.terrestria.feature.placer.trunk.BentTrunkPlacer;
-import com.terraformersmc.terrestria.feature.placer.trunk.CanopyTree4BranchTrunkPlacer;
-import com.terraformersmc.terrestria.feature.placer.trunk.IncrementedStraightTrunkPlacer;
-import com.terraformersmc.terrestria.feature.placer.trunk.QuarteredMegaIncrementedStraightTrunkPlacer;
+import com.terraformersmc.terrestria.feature.placer.trunk.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.trunk.TrunkPlacer;
 import net.minecraft.world.gen.trunk.TrunkPlacerType;
@@ -16,6 +13,7 @@ public class TerrestriaTrunkPlacerTypes {
 	public static TrunkPlacerType<BentTrunkPlacer> BENT;
 	public static TrunkPlacerType<CanopyTree4BranchTrunkPlacer> CANOPY_4_BRANCHES;
 	public static TrunkPlacerType<QuarteredMegaIncrementedStraightTrunkPlacer> QUARTERED_MEGA_TREE;
+	public static TrunkPlacerType<QuarteredMegaCanopyTrunkPlacer> QUARTERED_MEGA_CANOPY;
 	public static TrunkPlacerType<IncrementedStraightTrunkPlacer> INCREMENTED_STRAIGHT;
 
 	private static <P extends TrunkPlacer> TrunkPlacerType<P> register(String name, Codec<P> codec) {
@@ -26,6 +24,7 @@ public class TerrestriaTrunkPlacerTypes {
 		BENT = register("bent_trunk_placer", BentTrunkPlacer.CODEC);
 		CANOPY_4_BRANCHES = register("canopy_tree_4_branch_trunk_placer", CanopyTree4BranchTrunkPlacer.CODEC);
 		QUARTERED_MEGA_TREE = register("quartered_mega_trunk_placer", QuarteredMegaIncrementedStraightTrunkPlacer.CODEC);
+		QUARTERED_MEGA_CANOPY = register("quartered_mega_canopy_trunk_placer", QuarteredMegaCanopyTrunkPlacer.CODEC);
 		INCREMENTED_STRAIGHT = register("incremented_straight_trunk_placer", IncrementedStraightTrunkPlacer.CODEC);
 	}
 
