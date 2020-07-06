cbe77d69846a0761bcd2960363f107f7027b29a0
Brandon Davis <neusfear@gmail.com>
2020-07-06 12:51:22 -0500

    allow jungle palm, bryce, and yuuca palm saplings to be placed on sand

--- src/main/java/com/terraformersmc/terrestria/init/TerrestriaBlocks.java
+++ src/main/java/com/terraformersmc/terrestria/init/TerrestriaBlocks.java
@@ -159,7 +159,7 @@ public class TerrestriaBlocks {
 		// Saplings
 		AbstractBlock.Settings saplingSettings = AbstractBlock.Settings.of(Material.PLANT).noCollision().ticksRandomly().breakInstantly().sounds(BlockSoundGroup.GRASS);
 
-		BRYCE_SAPLING = TerrestriaRegistry.register("bryce_sapling", new TerraformSaplingBlock(new TerraformSaplingGenerator(() -> TerrestriaFeatureConfigs.BRYCE_TREE), saplingSettings));
+		BRYCE_SAPLING = TerrestriaRegistry.register("bryce_sapling", new TerraformDesertSaplingBlock(new TerraformSaplingGenerator(() -> TerrestriaFeatureConfigs.BRYCE_TREE), saplingSettings));
 		REDWOOD_SAPLING = TerrestriaRegistry.register("redwood_sapling", new TerraformSaplingBlock(new TerraformLargeSaplingGenerator(() -> TerrestriaFeatureConfigs.REDWOOD_TREE, () -> TerrestriaFeatureConfigs.MEGA_REDWOOD_TREE), saplingSettings));
 		HEMLOCK_SAPLING = TerrestriaRegistry.register("hemlock_sapling", new TerraformSaplingBlock(new TerraformLargeSaplingGenerator(() -> TerrestriaFeatureConfigs.HEMLOCK_TREE, () -> TerrestriaFeatureConfigs.MEGA_HEMLOCK_TREE), saplingSettings));
 		RUBBER_SAPLING = TerrestriaRegistry.register("rubber_sapling", new TerraformSaplingBlock(new TerraformSaplingGenerator(() -> TerrestriaFeatureConfigs.RUBBER_TREE), saplingSettings));
@@ -170,9 +170,9 @@ public class TerrestriaBlocks {
 		DARK_JAPANESE_MAPLE_SAPLING = TerrestriaRegistry.register("dark_japanese_maple_sapling", new TerraformSaplingBlock(new TerraformSaplingGenerator(() -> TerrestriaFeatureConfigs.DARK_JAPANESE_MAPLE_TREE), saplingSettings));
 		RAINBOW_EUCALYPTUS_SAPLING = TerrestriaRegistry.register("rainbow_eucalyptus_sapling", new TerraformSaplingBlock(new TerraformLargeSaplingGenerator(() -> TerrestriaFeatureConfigs.RAINBOW_EUCALYPTUS_SAPLING_TREE, () -> TerrestriaFeatureConfigs.RAINBOW_EUCALYPTUS_TREE), saplingSettings));
 		SAKURA_SAPLING = TerrestriaRegistry.register("sakura_sapling", new TerraformSaplingBlock(new TerraformSaplingGenerator(() -> TerrestriaFeatureConfigs.SAKURA_TREE), saplingSettings));
-		JUNGLE_PALM_SAPLING = TerrestriaRegistry.register("jungle_palm_sapling", new TerraformSaplingBlock(new TerraformSaplingGenerator(() -> TerrestriaFeatureConfigs.JUNGLE_PALM_TREE), saplingSettings));
+		JUNGLE_PALM_SAPLING = TerrestriaRegistry.register("jungle_palm_sapling", new TerraformDesertSaplingBlock(new TerraformSaplingGenerator(() -> TerrestriaFeatureConfigs.JUNGLE_PALM_TREE), saplingSettings));
 		SAGUARO_CACTUS_SAPLING = TerrestriaRegistry.register("saguaro_cactus_sapling", new TerraformDesertSaplingBlock(new TerraformSaplingGenerator(() -> TerrestriaFeatureConfigs.SAGUARO_CACTUS_FEATURE), saplingSettings, true));
-		YUCCA_PALM_SAPLING = TerrestriaRegistry.register("yucca_palm_sapling", new TerraformSaplingBlock(new TerraformSaplingGenerator(() -> TerrestriaFeatureConfigs.YUCCA_PALM_TREE), saplingSettings));
+		YUCCA_PALM_SAPLING = TerrestriaRegistry.register("yucca_palm_sapling", new TerraformDesertSaplingBlock(new TerraformSaplingGenerator(() -> TerrestriaFeatureConfigs.YUCCA_PALM_TREE), saplingSettings));
 
 		// Volcanic Island Blocks
 
