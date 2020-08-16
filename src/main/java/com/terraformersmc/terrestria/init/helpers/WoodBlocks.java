4415f9f23bdad4c6766369f2ea015487db4ba0d8
Prospector <prospectordev@gmail.com>
2020-08-16 04:36:08 -0700

    Remove redundant locateany, fix leaves suffocating, fix certain leaves not have hoe as a tool, added andisol grass path item, fix sheep not eating andisol grass, fix trees growing on sand setting dirt below, give small oak logs the oak logs tag for recipes and stuff, fix lang for potted yucca palm sapling #change "Leaves no longer suffocate entities" #change "Fixed certain leaves not easily broken by a hoe" #change "Fixed Andisol Grass Path not having an item" #change "Fixed Andisol Grass Blocks not being eaten by sheep" #change "Fixed sandy trees having dirt below them" #change "Fixed Andisol Grass Blocks spreading improperly" #change "Fixed small oak logs not having oak log recipes" #change "Fixed localization for Potted Yucca Palm Saplings"

--- src/main/java/com/terraformersmc/terrestria/init/helpers/WoodBlocks.java
+++ src/main/java/com/terraformersmc/terrestria/init/helpers/WoodBlocks.java
@@ -3,6 +3,7 @@ package com.terraformersmc.terrestria.init.helpers;
 import com.terraformersmc.terraform.block.*;
 import com.terraformersmc.terrestria.Terrestria;
 import com.terraformersmc.terrestria.block.TerrestriaOptiLeavesBlock;
+import com.terraformersmc.terrestria.init.TerrestriaBlocks;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
@@ -39,12 +40,12 @@ public class WoodBlocks {
 			if (size.equals(LogSize.SMALL)) {
 				throw new IllegalArgumentException("Small log trees are not compatible with extended leaves, I'm not sure how you even did this...");
 			}
-			blocks.leaves = TerrestriaRegistry.register(name + "_leaves", new TerrestriaOptiLeavesBlock(FabricBlockSettings.copyOf(Blocks.OAK_LEAVES).breakByTool(FabricToolTags.HOES).materialColor(colors.leaves)));
+			blocks.leaves = TerrestriaRegistry.register(name + "_leaves", new TerrestriaOptiLeavesBlock(FabricBlockSettings.copyOf(Blocks.OAK_LEAVES).breakByTool(FabricToolTags.HOES).materialColor(colors.leaves).allowsSpawning(TerrestriaBlocks::canSpawnOnLeaves).suffocates(TerrestriaBlocks::never).blockVision(TerrestriaBlocks::never)));
 		} else {
 			if (size.equals(LogSize.SMALL)) {
-				blocks.leaves = TerrestriaRegistry.register(name + "_leaves", new TransparentLeavesBlock(FabricBlockSettings.copyOf(Blocks.OAK_LEAVES).breakByTool(FabricToolTags.HOES).materialColor(colors.leaves)));
+				blocks.leaves = TerrestriaRegistry.register(name + "_leaves", new TransparentLeavesBlock(FabricBlockSettings.copyOf(Blocks.OAK_LEAVES).breakByTool(FabricToolTags.HOES).materialColor(colors.leaves).allowsSpawning(TerrestriaBlocks::canSpawnOnLeaves).suffocates(TerrestriaBlocks::never).blockVision(TerrestriaBlocks::never)));
 			} else {
-				blocks.leaves = TerrestriaRegistry.register(name + "_leaves", new LeavesBlock(FabricBlockSettings.copyOf(Blocks.OAK_LEAVES).breakByTool(FabricToolTags.HOES).materialColor(colors.leaves)));
+				blocks.leaves = TerrestriaRegistry.register(name + "_leaves", new LeavesBlock(FabricBlockSettings.copyOf(Blocks.OAK_LEAVES).breakByTool(FabricToolTags.HOES).materialColor(colors.leaves).allowsSpawning(TerrestriaBlocks::canSpawnOnLeaves).suffocates(TerrestriaBlocks::never).blockVision(TerrestriaBlocks::never)));
 			}
 		}
 
--- src/main/java/com/terraformersmc/terrestria/mixin/MixinEatGrassGoal.java
+++ src/main/java/com/terraformersmc/terrestria/mixin/MixinEatGrassGoal.java
@@ -26,32 +26,25 @@ public class MixinEatGrassGoal {
 	@Final
 	private World world;
 
-	@Shadow
-	private int timer;
-
 	@Inject(method = "canStart", at = @At(value = "FIELD", target = "Lnet/minecraft/block/Blocks;GRASS_BLOCK:Lnet/minecraft/block/Block;"), cancellable = true)
 	private void canStart(CallbackInfoReturnable<Boolean> callbackInfo) {
 		BlockPos pos = this.mob.getPositionTarget();
 
-		if(this.world.getBlockState(pos.down()).getBlock() == TerrestriaBlocks.ANDISOL_GRASS_BLOCK) {
+		if (this.world.getBlockState(pos.down()).getBlock() == TerrestriaBlocks.ANDISOL_GRASS_BLOCK) {
 			callbackInfo.setReturnValue(true);
 		}
 	}
 
-	@Inject(method = "tick", at = @At("HEAD"))
+	@Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/BlockPos;down()Lnet/minecraft/util/math/BlockPos;"))
 	private void tick(CallbackInfo info) {
-		if (this.timer - 1 == 4) {
-			BlockPos pos = this.mob.getPositionTarget();
-			BlockPos downPos = pos.down();
-
-			if (this.world.getBlockState(downPos).getBlock() == TerrestriaBlocks.ANDISOL_GRASS_BLOCK) {
-				if (this.world.getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING)) {
-					this.world.syncGlobalEvent(2001, downPos, Block.getRawIdFromState(Blocks.GRASS_BLOCK.getDefaultState()));
-					this.world.setBlockState(downPos, TerrestriaBlocks.ANDISOL.getDefaultState(), 2);
-				}
-
-				this.mob.onEatingGrass();
+		BlockPos pos = this.mob.getBlockPos().down();
+		if (this.world.getBlockState(pos).isOf(TerrestriaBlocks.ANDISOL_GRASS_BLOCK)) {
+			if (this.world.getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING)) {
+				this.world.syncWorldEvent(2001, pos, Block.getRawIdFromState(Blocks.GRASS_BLOCK.getDefaultState()));
+				this.world.setBlockState(pos, TerrestriaBlocks.ANDISOL.getDefaultState(), 2);
 			}
+
+			this.mob.onEatingGrass();
 		}
 	}
 }
