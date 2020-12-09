fa15f121e82c6a6e04e49460fbfe8bf126985872
coderbot <coderbot16@gmail.com>
2020-12-09 12:44:59 -0800

    Refactor TerrestriaConfigManager to be a bit more resilient to errors

--- src/main/java/com/terraformersmc/terrestria/config/TerrestriaConfigManager.java
+++ src/main/java/com/terraformersmc/terrestria/config/TerrestriaConfigManager.java
@@ -3,140 +3,126 @@ package com.terraformersmc.terrestria.config;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 import com.terraformersmc.terrestria.Terrestria;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
 
-import java.io.*;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class TerrestriaConfigManager {
-	private static File directory;
-	private static File clientFile;
-	private static File generalFile;
-	private static TerrestriaClientConfig clientConfig;
-	private static TerrestriaGeneralConfig generalConfig;
-	public static final Gson GSON = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).setPrettyPrinting().create();
+	private final Path clientConfigPath;
+	private final Path clientConfigBackupPath;
+	private final Path generalConfigPath;
+	private final Path generalConfigBackupPath;
+	private TerrestriaClientConfig clientConfig;
+	private TerrestriaGeneralConfig generalConfig;
 
-	static {
-		// Make sure the configuration is ready
-		initializeGeneralConfig();
-	}
+	private static final Gson GSON = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).setPrettyPrinting().create();
 
-	private static void prepareFiles() {
-		if (directory == null) {
-			directory = new File(FabricLoader.getInstance().getConfigDirectory(), Terrestria.MOD_ID);
-			if (!directory.exists()) {
-				directory.mkdir();
-			}
-		}
-		if (clientFile == null && FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
-			clientFile = new File(directory, "client.json");
-		}
-		if (generalFile == null) {
-			generalFile = new File(directory, "general.json");
-		}
-	}
-
-	public static TerrestriaClientConfig initializeClientConfig() {
-		if (FabricLoader.getInstance().getEnvironmentType() != EnvType.CLIENT) {
-			throw new UnsupportedOperationException("Cannot initialize the Terrestria client configuration when not on the client.");
-		}
-		if (clientConfig != null) {
-			return clientConfig;
-		}
-
-		clientConfig = new TerrestriaClientConfig();
-		loadClient();
-		return clientConfig;
-	}
-
-	public static TerrestriaGeneralConfig initializeGeneralConfig() {
-		if (generalConfig != null) {
-			return generalConfig;
-		}
-
-		generalConfig = new TerrestriaGeneralConfig();
-		loadGeneral();
-
-		return generalConfig;
-	}
-
-	private static void loadClient() {
-		if (FabricLoader.getInstance().getEnvironmentType() != EnvType.CLIENT) {
-			throw new UnsupportedOperationException("Cannot load the Terrestria client configuration when not on the client.");
-		}
-		prepareFiles();
+	public TerrestriaConfigManager() {
+		Path configDirectory = FabricLoader.getInstance().getConfigDir().resolve(Terrestria.MOD_ID);
 
 		try {
-			if (!clientFile.exists()) {
-				saveClient();
-			}
-			if (clientFile.exists()) {
-				BufferedReader br = new BufferedReader(new FileReader(clientFile));
-
-				clientConfig = GSON.fromJson(br, TerrestriaClientConfig.class);
-			}
-		} catch (FileNotFoundException e) {
-			System.err.println("Couldn't load Terrestria client configuration file; reverting to defaults");
-			e.printStackTrace();
+			Files.createDirectories(configDirectory);
+		} catch (IOException e) {
+			Terrestria.LOGGER.error("Failed to create config directory at " + configDirectory, e);
 		}
+
+		if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+			clientConfigPath = configDirectory.resolve("client.json");
+			clientConfigBackupPath = configDirectory.resolve("client-invalid-syntax.json");
+		} else {
+			clientConfigPath = null;
+			clientConfigBackupPath = null;
+		}
+
+		generalConfigPath = configDirectory.resolve("general.json");
+		generalConfigBackupPath = configDirectory.resolve("general-invalid-syntax.json");
 	}
 
-	private static void loadGeneral() {
-		prepareFiles();
+	private static <T> T loadConfig(Path configPath, T defaults, Class<T> clazz, Path backupPath) {
+		if (!Files.exists(configPath)) {
+			saveConfig(configPath, defaults);
+
+			if (!Files.exists(configPath)) {
+				Terrestria.LOGGER.error("Unable to save default configuration values for " + configPath);
+				Terrestria.LOGGER.error("Ensure that you have the correct file permissions and that your disk isn't full!");
+
+				// Not much else to do at this point.
+				return defaults;
+			}
+		}
+
+		String content;
 
 		try {
-			if (!generalFile.exists()) {
-				saveGeneral();
-			}
-			if (generalFile.exists()) {
-				BufferedReader br = new BufferedReader(new FileReader(generalFile));
+			// Load the entire file first, so that we don't get any weird IO errors halfway through.
+			byte[] bytes = Files.readAllBytes(configPath);
 
-				generalConfig = GSON.fromJson(br, TerrestriaGeneralConfig.class);
-			}
-		} catch (FileNotFoundException e) {
-			System.err.println("Couldn't load Terrestria general configuration file; reverting to defaults");
-			e.printStackTrace();
-		}
-	}
-
-	public static void saveClient() {
-		if (FabricLoader.getInstance().getEnvironmentType() != EnvType.CLIENT) {
-			throw new UnsupportedOperationException("Cannot save the Terrestria client configuration when not on the client.");
-		}
-		prepareFiles();
-
-		String jsonString = GSON.toJson(clientConfig);
-
-		try (FileWriter fileWriter = new FileWriter(clientFile)) {
-			fileWriter.write(jsonString);
+			// Make sure that we're using UTF-8. This prevents weird locale / OS issues.
+			content = new String(bytes, StandardCharsets.UTF_8);
 		} catch (IOException e) {
-			System.err.println("Couldn't save Terrestria client configuration file");
-			e.printStackTrace();
+			Terrestria.LOGGER.error("Failed to load Terrestria configuration file at " + configPath, e);
+			Terrestria.LOGGER.error("This shouldn't happen under normal conditions, ensure that you have the correct permissions");
+			Terrestria.LOGGER.error("Reverting to default configuration");
+
+			return defaults;
+		}
+
+		try {
+			return GSON.fromJson(content, clazz);
+		} catch (JsonSyntaxException e) {
+			Terrestria.LOGGER.error("Failed to parse Terrestria configuration file at " + configPath, e);
+			Terrestria.LOGGER.error("Reverting to default configuration, ensure that your file has correct syntax");
+			Terrestria.LOGGER.error("In the future, consider using something like https://jsonchecker.com/ to check your syntax");
+
+			// It would be quite annoying if a user just spent 10 minutes editing the file, only for it to be wiped away.
+			// Save a backup so they can review the errors and fix them.
+			Terrestria.LOGGER.error("The previous configuration file content has been written to " + backupPath);
+
+			try {
+				Files.write(backupPath, content.getBytes(StandardCharsets.UTF_8));
+			} catch (IOException ioe) {
+				Terrestria.LOGGER.error("Couldn't save previous configuration file content at " + backupPath, ioe);
+				Terrestria.LOGGER.error("This shouldn't happen under normal conditions, ensure that you have the correct permissions");
+			}
+
+			return defaults;
 		}
 	}
 
-	public static void saveGeneral() {
-		prepareFiles();
+	private static <T> void saveConfig(Path configPath, T instance) {
+		String jsonString = GSON.toJson(instance);
 
-		String jsonString = GSON.toJson(generalConfig);
-
-		try (FileWriter fileWriter = new FileWriter(generalFile)) {
-			fileWriter.write(jsonString);
+		try {
+			Files.write(configPath, jsonString.getBytes(StandardCharsets.UTF_8));
 		} catch (IOException e) {
-			System.err.println("Couldn't save Terrestria general configuration file");
-			e.printStackTrace();
+			Terrestria.LOGGER.error("Couldn't save Terrestria configuration file at " + configPath, e);
+			Terrestria.LOGGER.error("This shouldn't happen under normal conditions, ensure that you have the correct permissions");
 		}
 	}
 
-	public static TerrestriaClientConfig getClientConfig() {
+	public TerrestriaClientConfig getClientConfig() {
 		if (FabricLoader.getInstance().getEnvironmentType() != EnvType.CLIENT) {
 			throw new UnsupportedOperationException("Cannot get the Terrestria client configuration when not on the client.");
 		}
+
+		if (clientConfig == null) {
+			clientConfig = loadConfig(clientConfigPath, new TerrestriaClientConfig(), TerrestriaClientConfig.class, clientConfigBackupPath);
+		}
+
 		return clientConfig;
 	}
 
-	public static TerrestriaGeneralConfig getGeneralConfig() {
+	public TerrestriaGeneralConfig getGeneralConfig() {
+		if (generalConfig == null) {
+			generalConfig = loadConfig(generalConfigPath, new TerrestriaGeneralConfig(), TerrestriaGeneralConfig.class, generalConfigBackupPath);
+		}
+
 		return generalConfig;
 	}
 }
