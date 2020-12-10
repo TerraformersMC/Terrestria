3307ea5de82a54b1dd190625f8252bda6c0fdd83
coderbot <coderbot16@gmail.com>
2020-12-09 21:31:15 -0800

    Try to assign an appropriate villager type to each biome
    Fixes https://github.com/TerraformersMC/Terrestria/issues/204

--- src/main/java/com/terraformersmc/terrestria/Terrestria.java
+++ src/main/java/com/terraformersmc/terrestria/Terrestria.java
@@ -51,6 +51,7 @@ public class Terrestria implements ModInitializer {
 		TerrestriaSurfaces.init();
 		TerrestriaBiomes.init();
 		TerrestriaGeneration.init(config, enabledBiomes);
+		TerrestriaVillagerTypes.init();
 
 		biomeConfigHandler.save();
 
