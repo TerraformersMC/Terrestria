8ebbfc9beec19fb27bb3a19541dccc212347fc5b
coderbot <coderbot16@gmail.com>
2020-12-15 22:24:38 -0800

    Code style fixes to QuarteredMegaTreeConfig.CODEC

--- src/main/java/com/terraformersmc/terrestria/feature/tree/treeconfigs/QuarteredMegaTreeConfig.java
+++ src/main/java/com/terraformersmc/terrestria/feature/tree/treeconfigs/QuarteredMegaTreeConfig.java
@@ -15,28 +15,21 @@ import net.minecraft.world.gen.trunk.TrunkPlacer;
 
 public class QuarteredMegaTreeConfig extends TreeFeatureConfig {
 	// begin: vanilla copy
-	public static final Codec<QuarteredMegaTreeConfig> CODEC = RecordCodecBuilder.create((instance) -> {
-		return instance.group(
-				BlockStateProvider.TYPE_CODEC.fieldOf("trunk_provider").forGetter((treeFeatureConfig) -> {
-			return treeFeatureConfig.trunkProvider;
-		}), BlockStateProvider.TYPE_CODEC.fieldOf("leaves_provider").forGetter((treeFeatureConfig) -> {
-			return treeFeatureConfig.leavesProvider;
-		}), FoliagePlacer.TYPE_CODEC.fieldOf("foliage_placer").forGetter((treeFeatureConfig) -> {
-			return treeFeatureConfig.foliagePlacer;
-		}), TrunkPlacer.CODEC.fieldOf("trunk_placer").forGetter((treeFeatureConfig) -> {
-			return treeFeatureConfig.trunkPlacer;
-		}), FeatureSize.TYPE_CODEC.fieldOf("minimum_size").forGetter((treeFeatureConfig) -> {
-			return treeFeatureConfig.minimumSize;
-		}), TreeDecorator.TYPE_CODEC.listOf().fieldOf("decorators").forGetter((treeFeatureConfig) -> {
-			return treeFeatureConfig.decorators;
-		}), Codec.INT.fieldOf("max_water_depth").orElse(0).forGetter((treeFeatureConfig) -> {
-			return treeFeatureConfig.maxWaterDepth;
-		}), Codec.BOOL.fieldOf("ignore_vines").orElse(false).forGetter(config -> config.ignoreVines),
+	public static final Codec<QuarteredMegaTreeConfig> CODEC = RecordCodecBuilder.create (
+		instance -> instance.group (
+			BlockStateProvider.TYPE_CODEC.fieldOf("trunk_provider").forGetter(config -> config.trunkProvider),
+			BlockStateProvider.TYPE_CODEC.fieldOf("leaves_provider").forGetter(config -> config.leavesProvider),
+			FoliagePlacer.TYPE_CODEC.fieldOf("foliage_placer").forGetter(config -> config.foliagePlacer),
+			TrunkPlacer.CODEC.fieldOf("trunk_placer").forGetter(config -> config.trunkPlacer),
+			FeatureSize.TYPE_CODEC.fieldOf("minimum_size").forGetter(config -> config.minimumSize),
+			TreeDecorator.TYPE_CODEC.listOf().fieldOf("decorators").forGetter(config -> config.decorators),
+			Codec.INT.fieldOf("max_water_depth").orElse(0).forGetter(config -> config.maxWaterDepth),
+			Codec.BOOL.fieldOf("ignore_vines").orElse(false).forGetter(config -> config.ignoreVines),
 			Heightmap.Type.CODEC.fieldOf("heightmap").forGetter(config -> config.heightmap),
 			BlockStateProvider.TYPE_CODEC.fieldOf("quartered_trunk_provider").forGetter(config -> config.quarteredTrunkProvider),
 			BlockStateProvider.TYPE_CODEC.fieldOf("roots_provider").forGetter(config -> config.rootsProvider)
-		).apply(instance, QuarteredMegaTreeConfig::new);
-	});
+		).apply(instance, QuarteredMegaTreeConfig::new)
+	);
 	// end: vanilla copy
 
 	public final BlockStateProvider quarteredTrunkProvider;
