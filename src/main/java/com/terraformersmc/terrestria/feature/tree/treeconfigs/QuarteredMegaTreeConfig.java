61c46415f36bc309505a09b1ddfdf2e78b235e83
coderbot <coderbot16@gmail.com>
2020-12-15 21:47:38 -0800

    Initial work on getting quarter logs working in worldgen

--- src/main/java/com/terraformersmc/terrestria/feature/tree/treeconfigs/QuarteredMegaTreeConfig.java
+++ src/main/java/com/terraformersmc/terrestria/feature/tree/treeconfigs/QuarteredMegaTreeConfig.java
@@ -1,19 +1,63 @@
 package com.terraformersmc.terrestria.feature.tree.treeconfigs;
 
-import net.minecraft.block.BlockState;
+import java.util.List;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+import net.minecraft.world.Heightmap;
 import net.minecraft.world.gen.feature.TreeFeatureConfig;
+import net.minecraft.world.gen.feature.size.FeatureSize;
+import net.minecraft.world.gen.foliage.FoliagePlacer;
+import net.minecraft.world.gen.stateprovider.BlockStateProvider;
+import net.minecraft.world.gen.tree.TreeDecorator;
+import net.minecraft.world.gen.trunk.TrunkPlacer;
 
 public class QuarteredMegaTreeConfig extends TreeFeatureConfig {
+	// begin: vanilla copy
+	public static final Codec<QuarteredMegaTreeConfig> CODEC = RecordCodecBuilder.create((instance) -> {
+		return instance.group(
+				BlockStateProvider.TYPE_CODEC.fieldOf("trunk_provider").forGetter((treeFeatureConfig) -> {
+			return treeFeatureConfig.trunkProvider;
+		}), BlockStateProvider.TYPE_CODEC.fieldOf("leaves_provider").forGetter((treeFeatureConfig) -> {
+			return treeFeatureConfig.leavesProvider;
+		}), FoliagePlacer.TYPE_CODEC.fieldOf("foliage_placer").forGetter((treeFeatureConfig) -> {
+			return treeFeatureConfig.foliagePlacer;
+		}), TrunkPlacer.CODEC.fieldOf("trunk_placer").forGetter((treeFeatureConfig) -> {
+			return treeFeatureConfig.trunkPlacer;
+		}), FeatureSize.TYPE_CODEC.fieldOf("minimum_size").forGetter((treeFeatureConfig) -> {
+			return treeFeatureConfig.minimumSize;
+		}), TreeDecorator.TYPE_CODEC.listOf().fieldOf("decorators").forGetter((treeFeatureConfig) -> {
+			return treeFeatureConfig.decorators;
+		}), Codec.INT.fieldOf("max_water_depth").orElse(0).forGetter((treeFeatureConfig) -> {
+			return treeFeatureConfig.maxWaterDepth;
+		}), Codec.BOOL.fieldOf("ignore_vines").orElse(false).forGetter(config -> config.ignoreVines),
+			Heightmap.Type.CODEC.fieldOf("heightmap").forGetter(config -> config.heightmap),
+			BlockStateProvider.TYPE_CODEC.fieldOf("quartered_trunk_provider").forGetter(config -> config.quarteredTrunkProvider),
+			BlockStateProvider.TYPE_CODEC.fieldOf("roots_provider").forGetter(config -> config.rootsProvider)
+		).apply(instance, QuarteredMegaTreeConfig::new);
+	});
+	// end: vanilla copy
 
-	public final BlockState quarterLogBlock;
-	public final BlockState logBlock;
-	public final BlockState woodBlock;
+	public final BlockStateProvider quarteredTrunkProvider;
+	public final BlockStateProvider rootsProvider;
 
-	public QuarteredMegaTreeConfig(TreeFeatureConfig config, BlockState quarterLogBlock, BlockState logBlock, BlockState woodBlock) {
+	protected QuarteredMegaTreeConfig(BlockStateProvider trunkProvider, BlockStateProvider leavesProvider,
+	                            FoliagePlacer foliagePlacer, TrunkPlacer trunkPlacer, FeatureSize minimumSize, 
+	                            List<TreeDecorator> decorators, int maxWaterDepth, boolean ignoreVines, 
+	                            Heightmap.Type heightmap, BlockStateProvider quarteredTrunkProvider,
+	                            BlockStateProvider rootsProvider) {
+		super(trunkProvider, leavesProvider, foliagePlacer, trunkPlacer, minimumSize, decorators, maxWaterDepth,
+				ignoreVines, heightmap);
+
+		this.quarteredTrunkProvider = quarteredTrunkProvider;
+		this.rootsProvider = rootsProvider;
+	}
+	
+	public QuarteredMegaTreeConfig(TreeFeatureConfig config, BlockStateProvider quarteredTrunkProvider, BlockStateProvider rootsProvider) {
 		super(config.trunkProvider, config.leavesProvider, config.foliagePlacer, config.trunkPlacer, config.minimumSize, config.decorators, config.maxWaterDepth, config.ignoreVines, config.heightmap);
 
-		this.quarterLogBlock = quarterLogBlock;
-		this.logBlock = logBlock;
-		this.woodBlock = woodBlock;
+		this.quarteredTrunkProvider = quarteredTrunkProvider;
+		this.rootsProvider = rootsProvider;
 	}
 }
--- src/main/java/com/terraformersmc/terrestria/init/TerrestriaFeatures.java
+++ src/main/java/com/terraformersmc/terrestria/init/TerrestriaFeatures.java
@@ -1,8 +1,10 @@
 package com.terraformersmc.terrestria.init;
 
+import com.mojang.serialization.Codec;
 import com.terraformersmc.terrestria.Terrestria;
 import com.terraformersmc.terrestria.feature.CattailFeature;
 import com.terraformersmc.terrestria.feature.misc.DumDumHeadFeature;
+import com.terraformersmc.terrestria.feature.tree.treeconfigs.QuarteredMegaTreeConfig;
 
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -10,6 +12,7 @@ import net.minecraft.world.gen.ProbabilityConfig;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.FeatureConfig;
+import net.minecraft.world.gen.feature.TreeFeature;
 
 // This class exports public feature constants, these fields have to be public
 @SuppressWarnings("WeakerAccess")
@@ -17,10 +20,15 @@ public class TerrestriaFeatures {
 
 	public static CattailFeature CATTAIL;
 	public static Feature<DefaultFeatureConfig> DUM_DUM_HEAD;
+	public static TreeFeature QUARTERED_MEGA_TREE;
 
+	@SuppressWarnings({"rawtypes, unchecked"})
 	public static void init() {
 		CATTAIL = register("cattail", new CattailFeature(ProbabilityConfig.CODEC, TerrestriaBlocks.CATTAIL, TerrestriaBlocks.TALL_CATTAIL));
 		DUM_DUM_HEAD = register("dum_dum_head", new DumDumHeadFeature(DefaultFeatureConfig.CODEC));
+
+		// Super hacky casts, but it works
+		QUARTERED_MEGA_TREE = register("quartered_mega_tree", new TreeFeature((Codec) QuarteredMegaTreeConfig.CODEC));
 	}
 
 	public static <T extends Feature<FC>, FC extends FeatureConfig> T register(String name, T feature) {
