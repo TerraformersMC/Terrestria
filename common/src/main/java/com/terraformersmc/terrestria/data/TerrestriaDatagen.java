a6901a4412bdb4a7827433ac927f7c4068ff7c92
gniftygnome <minecraft@gnifty.net>
2022-06-19 18:07:06 -0700

    Data generation for biome tags.

---
+++ common/src/main/java/com/terraformersmc/terrestria/data/TerrestriaDatagen.java
@@ -0,0 +1,15 @@
+package com.terraformersmc.terrestria.data;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TerrestriaDatagen implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator dataGenerator) {
+		dataGenerator.addProvider(TerrestriaBiomeTagProvider::new);
+		//dataGenerator.addProvider(TerrestriaBlockLootTableProvider::new);
+		//dataGenerator.addProvider(TerrestriaBlockTagProvider::new);
+		//dataGenerator.addProvider(TerrestriaItemTagProvider::new);
+		//dataGenerator.addProvider(TerrestriaRecipeProvider::new);
+	}
+}
